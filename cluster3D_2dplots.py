c40986088d532d41af965cfdff2e3b8d1246f06f
Théo Cuisset <theo.cuisset@polytechnique.edu>
2023-03-15 15:40:04 +0100

    Rename Clus3DSpatialResolution

--- cluster3D_2dplots.py
+++ cluster3D_2dplots.py
@@ -8,7 +8,7 @@ s = Selectors()
 curdoc().add_root(row(
     s.makeWidgetColumnClue3D(),
     Tabs(tabs=[
-        s.tabClue3D("Spatial resolution", "Clus3DSpatialResolution", plotType="2d"),
+        s.tabClue3D("Spatial resolution", "Clus3DSpatialResolutionOf2DClusters", plotType="2d"),
         s.tabClue3D("Position X-Y", "Clus3DPositionXY",  plotType="2d"),
     ])
 ))
