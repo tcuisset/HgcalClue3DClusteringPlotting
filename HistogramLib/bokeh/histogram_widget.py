101f7127b930bcbae77055f15b3c9d656d8f7612
Théo Cuisset <theo.cuisset@polytechnique.edu>
2023-03-15 10:11:58 +0100

    Fix : plottedValueTitle issue

--- HistogramLib/bokeh/histogram_widget.py
+++ HistogramLib/bokeh/histogram_widget.py
@@ -86,7 +86,7 @@ class QuadHistogram2D(AbstractSingleHistogram):
             self.source.data = {"histogram_2D_view":[np.transpose(self.histProjectedView.getProjectedHistogramView())]}
         except HistogramLoadError:
             self.source.data = {"histogram_2D_view":[]}
-        self.figure.plottedValueTitle.text = self.metadata.getPlotLabel(self.histProjectedView.histKind)
+        self.plottedValueTitle.text = self.metadata.getPlotLabel(self.histProjectedView.histKind)
 
 
 class AbstractMultiHistogram(AbstractHistogram):
