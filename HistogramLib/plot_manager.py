f3e6f59acbd8388bfffb40033246a756ec0aae80
Théo Cuisset <theo.cuisset@polytechnique.edu>
2023-03-15 13:26:48 +0100

    Legend exploding plots

--- HistogramLib/plot_manager.py
+++ HistogramLib/plot_manager.py
@@ -1,4 +1,5 @@
 from typing import List, Tuple, Type, Callable
+from collections.abc import Iterable
 from itertools import product
 import copy
 
@@ -32,42 +33,55 @@ class PlotManager:
             selector.registerCallback(self.onSelectorCallback)
         self.makeEverything()
 
-    def _explode(self):
+    def _explode(self) -> Tuple[Selector, Iterable[tuple[tuple[Selection, int], ...]]]:
+        """ Return a all possible selections to plot according to the explode button
+        
+        Returns: tuple (overlaySelector, allSelections) where
+         - overlaySelector is a Selector, whose selections are to be plotted as an overlay plot (eventually None if no overlay is to be used)
+         - allSelections is a generator of tuples of tuples, each sub-tuple being of the form (Selection, lenSelections)
+             where lenSelections is the number of different Selection objects returned by the corresponding Selector
+             (1 -> no exploding of plots, 3 -> for example, result of data, sim_proton, sim_noproton being exploded)
+         """
         selectionsToExplode = []
         overlaySelector = None
         for selector in self.selectors:
             if self.multiPlotClass is not None and overlaySelector is None and len(selector.selections()) > 1 and selector.explodePlotType() is ExplodePlotType.OVERLAY:
                 overlaySelector = selector
             else:
-                selectionsToExplode.append(selector.selections())
+                selectionsToExplode.append([(selection, len(selector.selections())) for selection in selector.selections()])
         allSelections = product(*selectionsToExplode) # cartesian product
         return (overlaySelector, allSelections)
     
     def _updateModel(self):
         self.model.children = [plot.figure for plot in self.plots]
 
-    def makeMetadata(self, selections):
-        metadata = copy.copy(self.store.getMetadata(findHistNameInSelections(selections)))
+    def makeMetadata(self, selectionLengthTuple:Iterable[Tuple[Selection, int]]):
+        metadata = copy.copy(self.store.getMetadata(findHistNameInSelections([selection for selection, lenSelections in selectionLengthTuple])))
         slicedAxes = set()
-        for selection in selections:
+        for selection, lenSelections in selectionLengthTuple:
             if selection.slice() is not None and selection.slice().axisName in metadata.axes.name:
                 slicedAxes.add(selection.slice().axisName)
         metadata.axes = NamedAxesTuple(metadata.axes[axisName] for axisName in set(metadata.axes.name).difference(slicedAxes))
+        metadata.title += " "+" ".join(selection.label for selection, lenSelections in selectionLengthTuple if lenSelections > 1)
         return metadata
 
     def makeEverything(self):
         overlaySelector, allSelections = self._explode()
         self.plots = []
         firstFigure:figure = None
-        for selectionTuple in allSelections:
+        for selectionAndLengthTuple in allSelections: #selectionAndLengthTuple is tuple[tuple[Selection, int], ...]]
+            selectionList = [selection for selection, lenSelections in selectionAndLengthTuple]
             if overlaySelector is not None:
                 #Use for metadata the first overlay (otherwise the overlay axis is kept in metadata which is not wanted)
                 # TODO : have overlay axis in metadata
-                newPlot = self.multiPlotClass(metadata=self.makeMetadata(selectionTuple+(overlaySelector.selections()[0],)),
+
+                newPlot = self.multiPlotClass(
+                    # Append to selectionAndLengthTuple an element (with lenSelections=1)
+                    metadata=self.makeMetadata(selectionAndLengthTuple+((overlaySelector.selections()[0], 1),)),
                     projectedViews={
                         overlaySelection.label : HistogramView(
                             self.store,
-                            list(selectionTuple+(overlaySelection,))) # add overlay to tuple 
+                            list(selectionList+[overlaySelection])) # add overlay to tuple 
                         for overlaySelection in overlaySelector.selections()
                     }
                 )
@@ -78,10 +92,10 @@ class PlotManager:
                     figure_kwargs["x_range"] = firstFigure.x_range
                     figure_kwargs["y_range"] = firstFigure.y_range
                 
-                newPlot = self.singlePlotClass(metadata=self.makeMetadata(selectionTuple),
+                newPlot = self.singlePlotClass(metadata=self.makeMetadata(selectionAndLengthTuple),
                     projectedView=HistogramView(
                         self.store,
-                        list(selectionTuple)
+                        selectionList
                     ),
                     **figure_kwargs
                 )
@@ -89,7 +103,8 @@ class PlotManager:
                     firstFigure = newPlot.figure
 
             self.plots.append(newPlot)
-            self.lambdaOnPlotCreation(newPlot)
+            if self.lambdaOnPlotCreation is not None:
+                self.lambdaOnPlotCreation(newPlot)
         self._updateModel()
 
     def onSelectorCallback(self, triggerSelector:Selector, plotsHaveChanged=False):
