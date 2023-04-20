7e1a1488f37c3e2e07fb435a74d60a78995c4993
Théo Cuisset <theo.cuisset@polytechnique.edu>
2023-04-20 18:00:34 +0200

    Dash app add host=0.0.0.0

--- dash_event_visualizer_pod.sh
+++ dash_event_visualizer_pod.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-python dash_event_visualizer.py --port=8080 --no-debug --input-file="/eos/user/t/tcuisset/hgcal/testbeam18-clue3d/v33/cmssw/data/CLUE_clusters.root"
+python dash_event_visualizer.py --port=8080 --host=0.0.0.0 --no-debug --input-file="/eos/user/t/tcuisset/hgcal/testbeam18-clue3d/v33/cmssw/data/CLUE_clusters.root"
