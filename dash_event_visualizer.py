3f93f9e55f4cddade3165f4ac706b88af1905d57
Théo Cuisset <theo.cuisset@polytechnique.edu>
2023-05-04 15:46:37 +0200

    Monte Carlo support for event vis

--- dash_event_visualizer.py
+++ dash_event_visualizer.py
@@ -5,6 +5,8 @@ Environment variables to set :
 """
 import urllib.parse
 import os
+import glob
+import collections
 
 import dash
 from dash import Dash, html, Input, Output, State, dcc
@@ -26,18 +28,50 @@ if __name__ == "__main__":
         description="CLUE3D event visualizer using Dash",
     )
     parser.add_argument('-p', '--port', default=8051, help="Port to listen on")
-    parser.add_argument('-i', '--input-file', default="/eos/user/t/tcuisset/hgcal/testbeam18-clue3d/v33/cmssw/data/CLUE_clusters.root",
-        dest="input_file", help="Path to CLUE_clusters.root file (included)")
+    # Local : /data_cms_upgrade/cuisset/testbeam18/clue3d/v33/cmssw/data/CLUE_clusters.root
+    # EOS : "/eos/user/t/tcuisset/hgcal/testbeam18-clue3d/v33/cmssw/data/CLUE_clusters.root",
+    parser.add_argument('-i', '--input-file', default=None, 
+        dest="input_file", help="Path to CLUE_clusters.root file, in single-file mode (specify either -i or -I, not both)")
+    # EOS : "/eos/user/t/tcuisset/hgcal/testbeam18-clue3d/v33/"
+    # local : /data_cms_upgrade/cuisset/testbeam18/clue3d/v33/
+    parser.add_argument('-I', '--input-folder', default="/data_cms_upgrade/cuisset/testbeam18/clue3d/v33/",
+        dest="input_folder", help="Path to folders holding CLUE_clusters.root files. Beyond this path, subdirectories should exist in the form *clue-params*/*datatype*/CLUE_clusters.root. Incompatible with -i")
     parser.add_argument('-d', '--debug', action=argparse.BooleanOptionalAction, help="Enable debug mode", dest="debug", default=True)
     parser.add_argument('-H', '--host', dest="host", default=None,
         help="Host name to bin on (if not specified, use Dash default  which is env variable HOST or 127.0.0.1). On llruicms you should put 'llruicms01'")
     args = parser.parse_args()
     clueInputFile = args.input_file
+    clueInputFolder = args.input_folder
 else:
-    clueInputFile = os.environ["CLUE_INPUT_FILE"]
+    try:
+        clueInputFile = os.environ["CLUE_INPUT_FILE"]
+    except KeyError:
+        clueInputFile = None
+    try:
+        clueInputFolder = os.environ["CLUE_INPUT_FOLDER"]
+    except KeyError:
+        clueInputFolder = None
 
-# Local : /data_cms_upgrade/cuisset/testbeam18/clue3d/v33/cmssw/data/CLUE_clusters.root
-eventLoader = EventLoader(clueInputFile)
+if (clueInputFile is None and clueInputFolder is None) or (clueInputFile is not None and clueInputFolder is not None):
+    raise ValueError("You should specify either -i or -I (or set either CLUE_INPUT_FILE or CLUE_INPUT_FOLDER environment variables)")
+
+# Discover clue-params and datatypes
+availableSamples = collections.defaultdict(dict) # dict clueParam -> dict : datatype -> EventLoader
+if clueInputFolder is not None:
+    paths = glob.glob(os.path.join('*',      '*',  'CLUE_clusters.root'), root_dir=clueInputFolder)
+    for path in paths:
+        folderPath, _ = os.path.split(path) # folderPath = clueParams/datatype, _=CLUE_clusters.root
+        clueParam, datatype = os.path.split(folderPath)
+        availableSamples[clueParam][datatype] = EventLoader(os.path.join(clueInputFolder, path))
+else:
+    availableSamples["N/A"].append(("N/A", EventLoader(clueInputFile)))
+availableSamples:dict[str, dict[str, EventLoader]] = dict(availableSamples) # transform from defaultdict to dict to avoid accidentally adding keys
+
+clueParamsList = list(availableSamples.keys())
+try: # Put "cmssw" as first list element
+    clueParamsList.insert(0, clueParamsList.pop(clueParamsList.index("cmssw")))
+except ValueError:
+    pass
 
 app = Dash(__name__)
 server = app.server
@@ -48,6 +82,7 @@ cache = flask_caching.Cache(app.server, config={
 })
 
 legendDivStyle = {'flex': '0 1 auto', 'margin':"10px"}
+dropdownStyle = {'flex': '1 1 auto'}
 app.layout = html.Div([
     html.Div([
         dcc.Location(id="url", refresh=False), # For some reason  "callback-nav" works but False does not
@@ -55,12 +90,15 @@ app.layout = html.Div([
 
         html.H1(children='CLUE3D event visualizer (right-click to rotate, left-click to move)'),
         html.Div(children=[
+            html.Div("ClueParams :", style=legendDivStyle),
+            dcc.Dropdown(options=clueParamsList, id="clueParam", style=dropdownStyle),
+            dcc.Dropdown(id="datatype", style={"width":"200px"}),
             html.Div("Beam energy (GeV) :", style=legendDivStyle),
-            dcc.Dropdown(options=beamEnergies, value=20, id="beamEnergy", style={'flex': '1 1 auto'}),
+            dcc.Dropdown(options=beamEnergies, value=20, id="beamEnergy", style=dropdownStyle),
             html.Div("Ntuple number :", style=legendDivStyle),
-            dcc.Dropdown(id="ntupleNumber", style={'flex': '1 1 auto'}),
+            dcc.Dropdown(id="ntupleNumber", style=dropdownStyle),
             html.Div("Event :", style=legendDivStyle),
-            dcc.Dropdown(id="event", style={'flex': '1 1 auto'}),
+            dcc.Dropdown(id="event", style=dropdownStyle),
             html.Div("Layer (for layer view) :"),
             html.Div(dcc.Slider(min=1, max=28, step=1, value=10, id="layer"), style={"flex":"10 10 auto"}),
             dcc.Clipboard(id="link-copy-clipboard", title="Copy link", content="abc"),
@@ -84,19 +122,64 @@ app.layout = html.Div([
     
 ], style={'display': 'flex', 'flex-flow': 'column', "height":"100vh"})
 
+class FullEventID(collections.namedtuple("FullEventID", ["clueParam", "datatype", "beamEnergy", "ntupleNumber", "event"], defaults=[None]*5)):
+    @classmethod
+    def fromDict(cls, d:dict) -> "FullEventID":
+        return cls(**{key : d.get(key, cls._field_defaults[key]) for key in cls._fields})
+    
+    @classmethod
+    def fromCallbackContext(cls, args_grouping:dict) -> "FullEventID":
+        dictRes = dict()
+        for arg_dict in args_grouping:
+            # arg_dict is a dict with id->.., value->..., etc
+            if arg_dict.id in cls._fields:
+                dictRes[arg_dict.id] = arg_dict.value
+        return cls(**dictRes)
+
+    def toEventId(self) -> EventID:
+        return EventID(self.beamEnergy, self.ntupleNumber, self.event)
+    
+    def isFilled(self):
+        return not any(map(lambda x: x is None, self))
+
+@app.callback(
+    Output("datatype", "options"),
+    [Input("clueParam", "value")]
+)
+def update_datatypes(clueParam):
+    print("update_datatypes", dash.ctx.triggered_prop_ids, dash.ctx.inputs, flush=True)
+    try:
+        datatypes = list(availableSamples[clueParam].keys())
+        try: # Put "data" as first list element
+            datatypes.insert(0, datatypes.pop(datatypes.index("data")))
+        except ValueError:
+            pass
+        return datatypes
+    except:
+        return []
+
 @app.callback(
     Output("ntupleNumber", "options"),
-    [Input("beamEnergy", "value")]
+    [Input("clueParam", "value"), Input("datatype", "value"), Input("beamEnergy", "value")]
 )
-def update_ntupleNumber(beamEnergy):
-    return ntupleNumbersPerBeamEnergy[int(beamEnergy)]
+def update_ntupleNumber(clueParam, datatype, beamEnergy):
+    print("update_ntupleNumber", dash.ctx.triggered_prop_ids, dash.ctx.inputs, flush=True)
+    try:
+        ntuple_energy_pairs = availableSamples[clueParam][datatype].ntuplesEnergies
+        return list(ntuple_energy_pairs.ntupleNumber[ntuple_energy_pairs.beamEnergy == beamEnergy])
+    except:
+        return []
 
 @app.callback(
     Output("event", "options"),
-    [Input("ntupleNumber", "value")]
+    [Input("clueParam", "value"), Input("datatype", "value"), Input("beamEnergy", "value"), Input("ntupleNumber", "value")]
 )
-def update_available_events(ntupleNumber):
-    return eventLoader.tree.arrays(cut=f"(ntupleNumber == {ntupleNumber})", filter_name=["event"]).event
+def update_availableEvents(clueParam, datatype, beamEnergy, ntupleNumber):
+    print("update_availableEvents", dash.ctx.triggered_prop_ids, dash.ctx.inputs, flush=True)
+    try:
+        return list(availableSamples[clueParam][datatype].eventNumbersPerNtuple(beamEnergy, ntupleNumber))
+    except:
+        return []
 
 def makePlotClue3D(event:LoadedEvent):
     """ Returns a Plotly figure representing the CLUE3D vis from a loaded event """
@@ -133,11 +216,14 @@ def makePlotLongitudinalProfile(event:LoadedEvent):
 
 
 @app.callback(
-    [Output("beamEnergy", "value"), Output("ntupleNumber", "value"), Output("event", "value"), Output("layer", "value"), Output("plot_tabs", "value")],
+    [Output("clueParam", "value"), Output("datatype", "value"), Output("beamEnergy", "value"), Output("ntupleNumber", "value"), Output("event", "value"), Output("layer", "value"), Output("plot_tabs", "value")],
     [Input("url", "search")]
 )
 def simpleUrlUpdate(urlSearchValue):
     """ On initial load, set settings from URL"""
+    if not dash.ctx.triggered:
+        # not initial load : ignore
+        raise PreventUpdate()
     print("simpleUrlUpdate", dash.ctx.triggered_prop_ids, dash.ctx.inputs, flush=True)
     try:
         parsed_url_query = urllib.parse.parse_qs(urlSearchValue[1:]) # Drop the leading "?"
@@ -150,60 +236,66 @@ def simpleUrlUpdate(urlSearchValue):
         except:
             layer_value = dash.no_update
 
-        return int(parsed_url_query["beamEnergy"][0]), int(parsed_url_query["ntuple"][0]), int(parsed_url_query["event"][0]), layer_value, plot_tabs_value
-    except KeyError:
+        return parsed_url_query["clueParam"][0], parsed_url_query["datatype"][0], int(parsed_url_query["beamEnergy"][0]), int(parsed_url_query["ntuple"][0]), int(parsed_url_query["event"][0]), layer_value, plot_tabs_value
+    except (KeyError, ValueError) as e: # catch non-existing key or catch None
+        print("simpleUrlUpdate : failed parse")
+        print(e)
         raise PreventUpdate()
 
 @app.callback(
     Output("link-copy-clipboard", "content"),
     [   Input("link-copy-clipboard", "n_clicks"), State("url", "href"),
-        State("beamEnergy", "value"), State("ntupleNumber", "value"), State("event", "value"), State("layer", "value"), State("plot_tabs", "value")
+        State("clueParam", "value"), State("datatype", "value"), State("beamEnergy", "value"), State("ntupleNumber", "value"), State("event", "value"), State("layer", "value"), State("plot_tabs", "value")
     ]
 )
-def updateLinkClipboard(_, href, beamEnergy, ntuple, event, layer, plot_tab):
+def updateLinkClipboard(_, href, clueParam, datatype, beamEnergy, ntuple, event, layer, plot_tab):
     url_tuple = urllib.parse.urlparse(href)
-    url_query = dict(beamEnergy=beamEnergy, ntuple=ntuple, event=event, layer=layer, tab=plot_tab)
+    url_query = dict(clueParam=clueParam, datatype=datatype, beamEnergy=beamEnergy, ntuple=ntuple, event=event, layer=layer, tab=plot_tab)
     new_url_tuple = url_tuple._replace(query=urllib.parse.urlencode(url_query)) # namedtuple are immutable
     return urllib.parse.urlunparse(new_url_tuple)
 
 @cache.memoize(timeout=60*5) # cache for 5 minutes 
-def loadEvent(ntuple, eventNb) -> LoadedEvent:
-    if ntuple is not None and eventNb is not None:
-        return eventLoader.loadEvent(EventID(ntuple, eventNb))
-    raise RuntimeError()
+def loadEvent(fullEventId:FullEventID) -> LoadedEvent:
+    return availableSamples[fullEventId.clueParam][fullEventId.datatype].loadEvent(fullEventId.toEventId())
 
 @app.callback(
     Output("signal-event-ready", "data"),
-    [Input("ntupleNumber", "value"), Input("event", "value")],
+    [State("clueParam", "value"), State("datatype", "value"), State("beamEnergy", "value"), State("ntupleNumber", "value"), Input("event", "value")],
 )
-def loadEventCallback(ntuple, event):
-    print("loadEventCallback", dash.ctx.triggered_prop_ids, dash.ctx.inputs, flush=True)
-    if ntuple is not None and event is not None:
-        loadEvent(ntuple, event) # just load the cache, discard results
-    return dict(ntupleNumber=ntuple, event=event)
+def loadEventCallback(clueParam, datatype, beamEnergy, ntuple, event):
+    print("loadEventCallback", dash.ctx.triggered_prop_ids, dash.ctx.inputs)
+    fullEventID = FullEventID.fromCallbackContext(dash.ctx.args_grouping)
+    #print(dash.ctx.args_grouping)
+    print(fullEventID)
+    if fullEventID.isFilled():
+        
+        loadEvent(fullEventID) # just load the cache, discard results
+    return fullEventID._asdict()
+
+def isStorageValid(storage_eventId:dict) -> bool:
+    return (storage_eventId is not None) and len(storage_eventId) > 0
 
 @app.callback(
     [Output("plot_3D", "figure"), Output("plot_longitudinal-profile", "figure")],
     [Input("signal-event-ready", "data")]
 )
-def mainEventUpdate(storeData):
+def mainEventUpdate(storage_eventId):
     """ Main callback to update all the plots at the same time.
     layer is State as there is another callback updateOnlyLayerPlot to update just the layer view """
-    if storeData is None:
-        return None, None
-    eventNb, ntupleNumber = storeData["event"], storeData["ntupleNumber"]
-
     print("mainEventUpdate", dash.ctx.triggered_prop_ids, dash.ctx.inputs, flush=True)
-    print(flush=True)
-    if eventNb is None or ntupleNumber is None:
-        return None, None
+    if not isStorageValid(storage_eventId):
+        return {}, {}
+    fullEventID = FullEventID(**storage_eventId)
+    if not fullEventID.isFilled():
+        return {}, {}
+    
     try:
-        event = loadEvent(ntupleNumber, eventNb)
+        event = loadEvent(fullEventID)
         plot_3D = makePlotClue3D(event)
         plot_longitudinal = makePlotLongitudinalProfile(event)
-    except RuntimeError:
-        plot_3D = None
-        plot_longitudinal = None
+    except:
+        plot_3D = {}
+        plot_longitudinal = {}
     
     return plot_3D, plot_longitudinal
 
@@ -213,13 +305,17 @@ def mainEventUpdate(storeData):
     [Input("signal-event-ready", "data"), Input("layer", "value")],
     prevent_initial_call=True, # On intial loading the layer view is loaded by the main callback mainEventUpdate
 )
-def updateOnlyLayerPlot(storeData, layer):
+def updateOnlyLayerPlot(storage_eventId, layer):
     """ Small callback to only update the layer view """
-    eventNb, ntupleNumber = storeData["event"], storeData["ntupleNumber"]
+    if not isStorageValid(storage_eventId):
+        return {}
+    fullEventID = FullEventID(**storage_eventId)
+    if not fullEventID.isFilled():
+        return {}
     try:
-        event = loadEvent(ntupleNumber, eventNb)
-    except RuntimeError:
-        return None
+        event = loadEvent(fullEventID)
+    except:
+        return {}
     return makePlotLayer(event, layer)
 
 if __name__ == '__main__':
--- event_visualizer_plotly/utils.py
+++ event_visualizer_plotly/utils.py
@@ -1,7 +1,9 @@
 import math
 import collections
 import itertools
+import functools
 from functools import cached_property
+import weakref
 
 import uproot
 import awkward as ak
@@ -11,14 +13,98 @@ import plotly.graph_objects as go
 
 import hists.parameters
 
-EventID = collections.namedtuple("EventID", ["ntupleNumber", "event"])
+# Uniquely identify an event. For data, beamEnergy is not necessary and can be set to None.
+# For simulation, it is needed since ntupleNumber overlap for different beam energies
+EventID = collections.namedtuple("EventID", ["beamEnergy", "ntupleNumber", "event"])
+
+
+        
+
+def memoized_method(*lru_args, **lru_kwargs):
+    """Small code taken from https://stackoverflow.com/a/33672499 
+    So we can cache method results with cache that is per instance rather than per class
+    as using @functools.cache leads to memery leaks when used on an instance method"""
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapped_func(self, *args, **kwargs):
+            # We're storing the wrapped method inside the instance. If we had
+            # a strong reference to self the instance would never die.
+            self_weak = weakref.ref(self)
+            @functools.wraps(func)
+            @functools.lru_cache(*lru_args, **lru_kwargs)
+            def cached_method(*args, **kwargs):
+                return func(self_weak(), *args, **kwargs)
+            setattr(self, func.__name__, cached_method)
+            return cached_method(*args, **kwargs)
+        return wrapped_func
+    return decorator
+
+class DTypeFinder:
+    def __init__(self, event_array_dict:dict[str, np.ndarray]) -> None:
+        self.event_array_dict = event_array_dict
+    
+    @memoized_method(maxsize=None)
+    def bestDtypeFor(self, column:str):
+        if column == "index":
+            return column, np.min_scalar_type(len(self.event_array_dict["beamEnergy"]))
+        else:
+            return column, np.min_scalar_type(np.max(self.event_array_dict[column]))
+
+class EventIndex:
+    """ Index of events """
+    def __init__(self, event_array_dict:dict[str, np.ndarray], columns:list[str]) -> None:
+        """ 
+         - event_array_dict : dictionnary column name -> numpy array
+         - columns: list of columns, order of which to sort on
+        """
+        self.event_array_dict = event_array_dict
+        
+        dtypeFinder = DTypeFinder(event_array_dict)
+        self._index_structured_dtype = np.dtype([dtypeFinder.bestDtypeFor(column) for column in (columns + ["index"])])
+
+        index_array = np.arange(0, len(event_array_dict["event"]), dtype=self._index_structured_dtype["index"])
+        self.record_array = np.core.records.fromarrays(list(event_array_dict.values())+[index_array], dtype=self._index_structured_dtype)
+        self.record_array.sort()
+
+    def findRecord(self, columnValues:list):
+        """ Find a record, by the values of columns. The values in columnValues should match the order of columns given in __init__ 
+        Returns a numpy record with columns values and a record called "index" holding index in original array
+        """
+        value = np.record(tuple(columnValues + [0]), dtype=self._index_structured_dtype)
+        foundRecord = self.record_array[np.searchsorted(self.record_array, value)]
+        return foundRecord
+    
+    def _makeRecordFromColumnValues(self, firstColumnValues, min=True):
+        value_list = []
+        for (dtype, _), value in itertools.zip_longest(self._index_structured_dtype.fields.values(), firstColumnValues):
+            if value is None:
+                if min:
+                    value_list.append(np.iinfo(dtype).min)
+                else:
+                    value_list.append(np.iinfo(dtype).max)
+            else:
+                value_list.append(value)
+        return np.record(tuple(value_list), dtype=self._index_structured_dtype)
+    
+    def findRange(self, firstColumnValues:list) -> np.ndarray:
+        """ Returns a view over the range of records selected by column values given in firstColumnValues.
+        This should be a continuous subset from the beginning of columns, in same order.
+        """
+        record_lookup_begin = self._makeRecordFromColumnValues(firstColumnValues, min=True)
+        record_lookup_end = self._makeRecordFromColumnValues(firstColumnValues, min=False)
+        
+        indexMin, indexMax = np.searchsorted(self.record_array, record_lookup_begin, side="left"), np.searchsorted(self.record_array, record_lookup_end, side="right")
+        #return indexMin, indexMax
+        return self.record_array[indexMin:indexMax]
+
 
 class EventLoader:
     def __init__(self, pathToFile:str) -> None:
         """ pathToFile is path to CLUE_clusters.root file (included)"""
         self.file = uproot.open(pathToFile, object_cache=None, array_cache=None)
         self.tree:uproot.TTree = self.file["clusters"]
-        self.eventIndex = self._buildLocateEventIndex()
+        self.eventIndex = EventIndex(self.tree.arrays(filter_name=["beamEnergy", "ntupleNumber", "event"], library="np"),
+                                        columns=["beamEnergy", "ntupleNumber", "event"])
 
     @cached_property
     def clueParameters(self):
@@ -28,35 +114,19 @@ class EventLoader:
     def clue3DParameters(self):
         return self.file["clue3DParams"].members
 
-    def _buildLocateEventIndex(self):
-        # Make a dict of numpy arrays
-        event_array_dict = self.tree.arrays(filter_name=["ntupleNumber", "event"], library="np")
-
-        self._index_structured_dtype = np.dtype([("ntupleNumber", "uint16"), ("event", "uint32"), ('index', 'uint32')])
-        indexed_array = np.zeros(event_array_dict["event"].shape, dtype=self._index_structured_dtype)
-        indexed_array['ntupleNumber'] = event_array_dict["ntupleNumber"]
-        indexed_array["event"] = event_array_dict["event"]
-        indexed_array["index"] = np.arange(0, len(event_array_dict["event"]), dtype="uint32")
-        indexed_array.sort(order=["ntupleNumber", "event"]) # ignore index for sorting
-        return indexed_array
-
-
     def _locateEvent(self, eventId:EventID):
         """ Compute the index in the tree of the given event, using the event index """
-        indexInSortedArray = np.searchsorted(self.eventIndex, np.array([(eventId.ntupleNumber, eventId.event, 0)], dtype=self._index_structured_dtype), side='right')[0]
+        if eventId.beamEnergy is None:
+            #eventId = eventId._replace(beamEnergy=0)
+            raise ValueError("For now, you must specify beamEnergy")
         try:
-            foundEvent = self.eventIndex[indexInSortedArray]
-            if foundEvent["event"] == eventId.event and foundEvent["ntupleNumber"] == eventId.ntupleNumber:
-                return foundEvent["index"]
+            foundRecord = self.eventIndex.findRecord([eventId.beamEnergy, eventId.ntupleNumber, eventId.event])
+            #foundEvent = self.eventIndex[foundRecord.index]
+            if (foundRecord["event"] == eventId.event and foundRecord["ntupleNumber"] == eventId.ntupleNumber 
+                and (eventId.beamEnergy == 0 or foundRecord["beamEnergy"] == eventId.beamEnergy)) :
+                return foundRecord["index"]
         except IndexError:
             pass
-        return None
-
-    def _locateEventUsingUproot(self, eventId:EventID) -> uproot.behaviors.TBranch.Report:
-        for array, report in self.tree.iterate(["ntupleNumber", "event"], report=True, library="np",
-            cut=f"(ntupleNumber == {eventId.ntupleNumber}) & (event=={eventId.event})", step_size=1000):
-            if len(array["ntupleNumber"]) > 0:
-                return report
         return None
 
     def loadEvent(self, eventId:EventID) -> "LoadedEvent": # (forward reference to LoadedEvent)
@@ -65,14 +135,19 @@ class EventLoader:
             raise RuntimeError("Could not find event")
         eventList = self.tree.arrays(entry_start=eventLocation, entry_stop=eventLocation+1)
         if len(eventList) > 1:
-            print("WARNING : duplicate event numbers") # should not happen
+            raise RuntimeError("Uproot problem")
+            #print("WARNING : duplicate event numbers") # should not happen 
         if len(eventList) < 1:
             raise RuntimeError("Could not find event") 
         return LoadedEvent(eventList[0], self)
     
-    def getNtuplesEnergies(self) -> ak.Array:
+    @cached_property
+    def ntuplesEnergies(self) -> ak.Array:
         """ Gets a list of records with the unique ntupleNumber and beamEnergy pairs"""
         return np.unique(self.tree.arrays(["ntupleNumber", "beamEnergy"]))
+
+    def eventNumbersPerNtuple(self, beamEnergy, ntupleNumber) -> np.ndarray:
+        return self.eventIndex.findRange([beamEnergy, ntupleNumber]).event
 
 class LoadedEvent:
     def __init__(self, record:ak.Record, el:EventLoader) -> None:
