8ced282f3c92151191a1b36b9bf8255747c7e852
Théo Cuisset <theo.cuisset@polytechnique.edu>
2023-06-02 11:50:40 +0200

    Update ntuple reader computation

--- ntupleReaders/computation.py
+++ ntupleReaders/computation.py
@@ -11,21 +11,27 @@ class BaseComputation:
         pass
 
 
-def computeAllFromTree(tree:uproot.TTree|list[uproot.TTree], computations:list[BaseComputation], count=None):
+def computeAllFromTree(tree:uproot.TTree|list[uproot.TTree], computations:list[BaseComputation], tqdm_options:dict=dict()):
+    """ Process all given Computation objects from all events in tree 
+    Parameters : 
+     - tree : an uproot TTree to process, or a list of them
+     - computations : a list of computations (they must satifsfy the BaseComputation model)
+     - tqdm_options : dict of keyword args passed to tqdm. You can pass for example desc, or count (in case you provide an generator for tree which has no __len__)
+     """
     neededBranches = set()
     for comp in computations:
         neededBranches.update(comp.neededBranches)
     
 
     if isinstance(tree, uproot.TTree):
-        with tqdm(total=tree.num_entries) as pbar:
+        with tqdm(total=tree.num_entries, **tqdm_options) as pbar:
             for (array, report) in tree.iterate(step_size="200MB", library="ak", report=True, filter_name=list(neededBranches)):
                 for comp in computations:
                     comp.process(array)
                 pbar.update(report.stop-report.start)
     else:
         for array in tqdm.tqdm(uproot.iterate(tree, step_size="200MB", library="ak", report=False, filter_name=list(neededBranches)),
-                            total=count):
+                            **tqdm_options):
             for comp in computations:
                 comp.process(array)
 
